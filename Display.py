18918d583f2b4f207eed8aa53fc5cbeb66d48c71
Mustard1 <45611195+Mustard1@users.noreply.github.com>
2019-04-08 15:33:34 -0500

    Changed some output
    Added an actual disclaimer and try to change the ascii art. 
    Shits broken though I'll fix at a later time

--- Display.py
+++ Display.py
@@ -3,19 +3,20 @@
 #Additionally design is not permanent and will be updated periodically
 
 def display_welcome():
-    print(" $$$$$$\   $$$$$$\  $$\                 $$\   $$\  $$$$$$\  $$\   $$\  $$$$$$\ \n" +
-          "$$  __$$\ $$  __$$\ $$ |                $$ |  $$ |$$  __$$\ $$ | $$  |$$  __$$\ \n" +
-          "$$ /  \__|$$ /  $$ |$$ |       $$$$$$$\ $$ |  $$ |$$ /  \__|$$ |$$  / $$ /  \__| \n" +
-          "\$$$$$$\  $$ |  $$ |$$ |      $$  _____|$$ |  $$ |$$ |      $$$$$  /  \$$$$$$\ \n" +
-          " \____$$\ $$ |  $$ |$$ |      \$$$$$$\  $$ |  $$ |$$ |      $$  $$<    \____$$\ \n" +
-          "$$\   $$ |$$ $$\$$ |$$ |       \____$$\ $$ |  $$ |$$ |  $$\ $$ |\$$\  $$\   $$ | \n" +
-          "\$$$$$$  |\$$$$$$ / $$$$$$$$\ $$$$$$$  |\$$$$$$  |\$$$$$$  |$$ | \$$\ \$$$$$$  | \n" +
-          " \______/  \___$$$\ \________|\_______/  \______/  \______/ \__|  \__| \______/  \n" +
-          "               \___|                                                             \n" +
-
-          "Disclaimer: Any and all exploits present here are used at the discretion of the user \n" +
+    print("\n\n\n" +
+	  "  ____    _____   __               __  __  ____     __  __      \n" +
+	  " /\  _`\ /\  __`\/\ \             /\ \/\ \/\  _`\  /\ \/\ \     \n" +
+	  " \ \,\L\_\ \ \/\ \ \ \        ____\ \ \ \ \ \ \/\_\\ \ \/'/'    \n" +
+	  "  \/_\__ \\ \ \ \ \ \ \  __  /',__\\ \ \ \ \ \ \/_/_\ \ , <     \n" +
+	  "     /\ \L\ \ \ \\'\\ \ \L\ \/\__, `\\ \ \_\ \ \ \L\ \\ \ \\`\   \n" +
+	  "   \ `\____\ \___\_\ \____/\/\____/ \ \_____\ \____/ \ \_\ \_\ \n" +
+	  "    \/_____/\/__//_/\/___/  \/___/   \/_____/\/___/   \/_/\/_/ \n" +
+          "                                                               \n\n" +
+          "Legal Disclaimer: Any and all exploits present here are used at the discretion of the user\n" +
+	  "Additionally, it is the user's responsibility to obey all applicable law, local, state and federal\n" +
+	  "All developers involved assume no liability and are not responsible for misuse or damage due to the use of this software\n" +
           "Latest Version can be found at https://github.com/Mustard1/SQLsUCK \n" +
-          "For help with usage enter -h or -help for usage and flags/options available")
+          "For help with usage enter -h or -help for usage and flags/options available\n\n\n")
 
 def display_help():
     print("Takes [OPTIONS] [URL] {payload name} \n" +
